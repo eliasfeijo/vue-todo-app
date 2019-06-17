ff4e54426a87bb3639f92df510ff9b088e1f4260
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-17 04:02:05 -0300

    Adding TodoList component.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <div id="app">
+    <TodoList v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList.vue';
+
 export default {
   name: 'app',
   components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo 1',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Todo 2',
+          completed: true,
+        },
+        {
+          id: 3,
+          title: 'Todo 3',
+          completed: false,
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-list">
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: ['todos'],
+};
+</script>
